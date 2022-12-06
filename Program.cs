85cb918d5640726851e7e6be31002ca511d83374
BreyneWard <wardbreyne@hotmail.com>
2022-12-06 19:26:22 +0100

    5.6. Schrikkeljaar

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _5._6._schrikkeljaar_bis;
+class Program
+{
+    static void Main(string[] args)
+    {
+         Console.WriteLine($"Geef een jaartal op:");
+        string input = Console.ReadLine();
+        int jaartal; 
+        while (string.IsNullOrEmpty(input) || !int.TryParse(input, out jaartal) || jaartal <= 0)
+        {
+            Console.WriteLine($"Input is empty, null, negative or not a number!, Give up a whole number: ");
+            input = Console.ReadLine();
+        }
+        Console.WriteLine("{0} is {1}een schrikkeljaar", jaartal, CalculateLeapYear(jaartal) ? "" : "g");
+    }
+
+    // Methods
+    static bool CalculateLeapYear(int jaartal)
+    {
+        if (jaartal % 4 == 0 && !(jaartal % 100 == 0))
+        {
+            return true;
+        }
+        else if (jaartal % 4 == 0 && jaartal % 100 == 0 && jaartal % 400 == 0 )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+       
+    }
+}
